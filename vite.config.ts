3eafd7add619132a0fc69167fa6da0ba35159269
Antonio Vizuete <antonio.vizuete@lansweeper.com>
2022-10-25 22:19:05 +0200

    :zap: Improve chunks size

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ export default defineConfig({
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
   },
+  build: {
+    rollupOptions: {
+      manualChunks: {
+        "monaco-editor": ["monaco-editor"],
+        "blueprintjs-core": ["@blueprintjs/core"],
+        "blueprintjs-icons": ["@blueprintjs/icons"],
+        "blueprintjs-table": ["@blueprintjs/table"],
+        "blueprintjs-popover": ["@blueprintjs/popover2"],
+      },
+    },
+  },
 });
