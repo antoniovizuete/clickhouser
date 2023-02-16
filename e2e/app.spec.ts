af908499b7d2198dbb1618e1e4d9e0163f7f06a3
Antonio Vizuete <antonio.vizuete@lansweeper.com>
2023-02-16 07:00:04 +0100

    :white_check_mark: Adapt test to the new navbar

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -13,13 +13,5 @@ test("Smoke test", async ({ page }) => {
 
   await expect(page.locator("button[aria-label='Run query']")).toBeVisible();
 
-  await expect(page.locator("button[aria-label='Run query']")).toBeEnabled();
-
-  await expect(page.getByPlaceholder("Server address")).toHaveValue(
-    "http://localhost:8123/"
-  );
-
-  await expect(page.getByPlaceholder("Username")).toHaveValue("default");
-
-  await expect(page.getByPlaceholder("Password")).toHaveValue("");
+  await expect(page.locator("button[aria-label='Run query']")).toBeDisabled();
 });
